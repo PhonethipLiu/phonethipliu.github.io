d2d346bd631fb569026a47c0bad5bbf18b337ba6
Phonethip <phonethip.liu@gmail.com>
2018-07-01 12:33:41 -0500

    adding nav.js file to create a module for reuse

---
+++ js/navbar.js
@@ -0,0 +1,32 @@
+<nav>
+               
+      <div class="nav-image"> 
+            <a href="#" class="border navbar-brand" id="nav-header-logo" style="background-color:transparent; color: #f90081;"> PHONETHIP </a>
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"> </span>
+            </button> 
+          </div>
+          
+          <div class="collapse navbar-collapse nav-header" id="navbarNavAltMarkup">
+            
+            <div class="navbar-nav">
+              <a href="#" class="border navbar-brand" id="nav-header-logo" style="background-color:transparent; color: #f90081;"> PHONETHIP </a>
+    
+              <a href="./html/aboutMe.html" class="nav-header-item flex-sm-column" >ABOUT ME</a>
+              
+              <div class="dropdown" aria-labelledby="navbarDropdownMenuLink" >
+                
+                <a href="#" class="nav-header-item dropdown-toggle flex-sm-column" id="navbarDropdownMenuLink" 
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  > WORK</a> 
+                  
+                  <div class="dropdown-menu flex-sm-column">
+                    <a class="dropdown-item" href="./Quizit.html">Quiz-it</a>
+                    <a class="dropdown-item" href="#">Delish</a>
+                    <a class="dropdown-item" href="#">Crime Checker</a>
+                    <a class="dropdown-item" href="#">Hustle</a>
+                  </div>
+              </div>
+            
+    
+        </nav>
